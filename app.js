5e9816f77015d87a33100e71ca774f4567eb3ba5
Tasha <xtasherx@gmail.com>
2020-08-26 00:11:00 -0500

    added cross-origin api

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ $(document).ready(function () {
   }
   // function to get weather info and write it to page
   function populateWeather() {
-    let responseURL = `http://api.openweathermap.org/data/2.5/weather?q=${userSearch}&units=imperial&appid=d4f35f1397cda8a7222b2b4264b60559`;
+    let responseURL = ` https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?q=${userSearch}&units=imperial&appid=d4f35f1397cda8a7222b2b4264b60559`;
     $.ajax({
       url: responseURL,
       method: "GET",
@@ -47,7 +47,7 @@ $(document).ready(function () {
       $(".display-3").text(response.name);
       const latHolder = response.coord.lat;
       const lonHolder = response.coord.lon;
-      responseURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${latHolder}&lon=${lonHolder}&units=imperial&appid=d4f35f1397cda8a7222b2b4264b60559`;
+      responseURL = ` https://cors-anywhere.herokuapp.com/https://api.openweathermap.org/data/2.5/onecall?lat=${latHolder}&lon=${lonHolder}&units=imperial&appid=d4f35f1397cda8a7222b2b4264b60559`;
 
       $.ajax({
         url: responseURL,
